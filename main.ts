d4bd1952113bff5087358bef8c7e7d58e90fdc53
DevonX <devonx1@gmail.com>
2024-05-11 11:57:24 +0200

    Updated main components and configurations

--- main.ts
+++ main.ts
@@ -1,134 +1,107 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, App, Setting, PluginSettingTab } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+class ExamplePluginSettings {
+	dateFormat: string;
+	tags: string[] = [];
 }
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class examplePlugin extends Plugin {
+	settings: ExamplePluginSettings;
+	
 	async onload() {
-		await this.loadSettings();
+		// Add settings tab
+		this.settings = Object.assign({}, await this.loadData(), {dateFormat: 'defaultFormat'});
+        this.addSettingTab(new ExampleSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const button = createEl("button", {cls: "your-button-css-class"});
+		const buttonSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>';
+		button.innerHTML = buttonSVG;
+		button.onclick = () => {
+			/* Do something */
+			console.log("BUTTON CLICKED");
+		}
+		
+		// Search for the place to add the button to, in this case, the main app container
+		const target = document.getElementsByClassName( "app-container")[0];
+		target.appendChild(button);
 	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
 	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        await this.saveData(this.settings);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ExampleSettingTab extends PluginSettingTab {
+	plugin: examplePlugin;
+	tagList: HTMLUListElement;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: examplePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+		const inputContainer = containerEl.createEl('div');
+		inputContainer.style.display = 'flex';
+		inputContainer.style.alignItems = 'center';
+
+		const tagNameInput = new Setting(inputContainer)
+			.setName('Tag name')
+			.setDesc('Enter a tag name here')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				.setPlaceholder('Tag name here')
+				.setValue(''));
+
+		const addButton = inputContainer.createEl('button', { text: 'Add Tag' });
+		addButton.style.marginLeft = '10px';
+
+		addButton.onclick = () => {
+			const tagNameInputValue = tagNameInput.settingEl.querySelector('input');
+			if (tagNameInputValue) {
+				const tagName = tagNameInputValue.value;
+
+				if (tagName) {
+					this.plugin.settings.tags.push(tagName);
+					this.plugin.saveSettings();
+					this.addTagToList(tagName);
+				}
+
+				// Reset the input value after adding a tag
+				tagNameInputValue.value = '';
+			}
+		};
+
+		this.tagList = containerEl.createEl('ul');
+
+		// Load the tags from the settings
+		for (const tag of this.plugin.settings.tags) {
+			this.addTagToList(tag);
+		}
+	}
+
+	addTagToList(tag: string): void {
+		const listItem = this.tagList.createEl('li');
+		listItem.style.marginBottom = '15px';
+		listItem.createEl('span', { text: tag });
+
+		const removeButton = listItem.createEl('button', { text: 'Remove' });
+		removeButton.style.float = 'right';
+		removeButton.style.marginLeft = '15px';
+
+		removeButton.onclick = () => {
+			const index = this.plugin.settings.tags.indexOf(tag);
+			if (index > -1) {
+				this.plugin.settings.tags.splice(index, 1);
+				this.plugin.saveSettings();
+			}
+			listItem.remove();
+		};
+	}
+
+	onunload() {
+		this.plugin.settings.tags = Array.from(this.tagList.children).map((child: HTMLElement) => child.textContent || '');
+		this.plugin.saveSettings();
 	}
 }
