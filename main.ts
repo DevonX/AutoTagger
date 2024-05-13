6d47c680be950916b02d62ec7c0cc1f92be1faae
DevonX <devonx1@gmail.com>
2024-05-13 22:56:03 +0200

    Added suggestions to text input, still bugs with the array search

--- main.ts
+++ main.ts
@@ -1,74 +1,104 @@
-import { Plugin, App, Setting, PluginSettingTab, ButtonComponent, TextComponent } from 'obsidian';
+import { Plugin, App, PluginSettingTab, ButtonComponent, TextComponent, SuggestModal} from 'obsidian';
 
 class ExamplePluginSettings {
-	dateFormat: string;
-	tags: string[] = [];
+    dateFormat: string;
+    tags: string[] = [];
 }
+
 export default class examplePlugin extends Plugin {
-	settings: ExamplePluginSettings;
-	
-	async onload() {
-		// Add settings tab
-		this.settings = Object.assign({}, await this.loadData(), {dateFormat: 'defaultFormat'});
+    settings: ExamplePluginSettings;
+
+    async onload() {
+        // Load settings
+        this.settings = Object.assign({}, await this.loadData(), { dateFormat: 'defaultFormat' });
         this.addSettingTab(new ExampleSettingTab(this.app, this));
 
-		const button = createEl("button", {cls: "your-button-css-class"});
-		const buttonSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>';
-		button.innerHTML = buttonSVG;
-		button.onclick = () => {
-			/* Do something */
-			console.log("BUTTON CLICKED");
-		}
-		
-		// Search for the place to add the button to, in this case, the main app container
-		const target = document.getElementsByClassName( "app-container")[0];
-		target.appendChild(button);
-	}
-	async saveSettings() {
+        // Add button to the app container
+        const button = createEl("button", { cls: "your-button-css-class" });
+        const buttonSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>';
+        button.innerHTML = buttonSVG;
+        button.onclick = () => {
+            console.log("BUTTON CLICKED");
+        }
+
+        const target = document.getElementsByClassName("app-container")[0];
+        target.appendChild(button);
+    }
+
+    async saveSettings() {
         await this.saveData(this.settings);
+    }
+}
+
+class TagSuggestModal extends SuggestModal<string> {
+    private suggestions: string[];
+    private onSelect: (value: string) => void;
+
+    constructor(app: App, suggestions: string[], onSelect: (value: string) => void) {
+        super(app);
+        this.suggestions = suggestions;
+        this.onSelect = onSelect;
+    }
+    getSuggestions(query: string): string[] {
+        return this.suggestions.filter(tag => tag.toLowerCase().includes(query.toLowerCase()));
+    }
+
+    renderSuggestion(suggestion: string, el: HTMLElement): void {
+        el.createEl("div", { text: suggestion });
+    }
+
+    onChooseSuggestion(suggestion: string, evt: MouseEvent | KeyboardEvent): void {
+        this.onSelect(suggestion);
     }
 }
 
 class ExampleSettingTab extends PluginSettingTab {
     plugin: examplePlugin;
-    tagList: HTMLUListElement;
-    
+    tagList: HTMLElement;
+
     constructor(app: App, plugin: examplePlugin) {
         super(app, plugin);
         this.plugin = plugin;
-		console.log('ExampleSettingTab constructor:', app, plugin);
     }
 
-    display(): void {
-		console.log('Display called');
+    async display(): Promise<void> {
         const { containerEl } = this;
 
         containerEl.empty();
 
-        const inputContainer = containerEl.createEl('div', {cls: "inputContainer"});
+        const inputContainer = containerEl.createDiv({ cls: "inputContainer" });
 
-        let tagNameTextComponent: TextComponent;
+        const files = this.app.vault.getMarkdownFiles();
+        const tags: string[] = [];
 
-        new Setting(inputContainer)
-            .setName('Tag name')
-            .setDesc('Enter a tag name here')
-            .addText(text => {
-                text.setPlaceholder('Tag name here')
-                    .setValue('');
-                tagNameTextComponent = text;
-				console.log('tagNameTextComponent:', tagNameTextComponent);
-            });
+        for (const file of files) {
+            const cache = await this.app.metadataCache.getCache(file.path);
+            if (cache && cache.tags) {
+                tags.push(...cache.tags.map(tag => tag.tag));
+            }
+        }
+
+        const tagNameTextComponent = new TextComponent(inputContainer);
+        tagNameTextComponent.setPlaceholder('Tag name here').setValue('');
+
+        tagNameTextComponent.inputEl.addEventListener('input', () => {
+            const value = tagNameTextComponent.getValue();
+            if (value.trim() !== '') {
+                new TagSuggestModal(this.app, tags, (suggestion) => {
+                    tagNameTextComponent.setValue(suggestion);
+                }).open();
+            }
+        });
 
         new ButtonComponent(inputContainer)
             .setButtonText('Add Tag')
             .onClick(() => {
                 const tagNameInputValue = tagNameTextComponent.getValue();
-				console.log('onClick:', tagNameInputValue, this.plugin.settings.tags);
 
                 if (tagNameInputValue) {
                     this.plugin.settings.tags.push(tagNameInputValue);
                     tagNameTextComponent.setValue('');
-                    this.displayTags(); // Call the displayTags method to update the tag list
+                    this.displayTags();
                     this.plugin.saveSettings();
                 }
             });
@@ -78,27 +108,23 @@ class ExampleSettingTab extends PluginSettingTab {
     }
 
     displayTags() {
-		console.log('displayTags called');
         this.tagList.empty();
-		console.log('tagList after empty:', this.tagList);
 
-		if (!this.plugin.settings.tags) {
-			this.plugin.settings.tags = [];
-		}
+        if (!this.plugin.settings.tags) {
+            this.plugin.settings.tags = [];
+        }
 
         this.plugin.settings.tags.forEach(tag => {
-			console.log('Current tag:', tag);
-            const tagItem = this.tagList.createEl('li', {text: tag, cls: 'listMargin'});
-            const deleteButton = tagItem.createEl('button', {cls: 'removeButton'});
+            const tagItem = this.tagList.createEl('li', { text: tag, cls: 'listMargin' });
+            const deleteButton = tagItem.createEl('button', { cls: 'removeButton' });
 
-			new ButtonComponent(deleteButton)
-				.setButtonText('Delete')
-				.onClick(() => {
-					this.plugin.settings.tags = this.plugin.settings.tags.filter(t => t !== tag);
-					console.log('Tags after deletion:', this.plugin.settings.tags);
-					this.displayTags();
-					this.plugin.saveSettings();
-            });
+            new ButtonComponent(deleteButton)
+                .setButtonText('Delete')
+                .onClick(() => {
+                    this.plugin.settings.tags = this.plugin.settings.tags.filter(t => t !== tag);
+                    this.displayTags();
+                    this.plugin.saveSettings();
+                });
         });
     }
 }
